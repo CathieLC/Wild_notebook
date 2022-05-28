85b5194b41c5af40634eb0416e6544544a381fa1
CathieLC <catherine-le-calve_student2021@wilder.school>
2022-05-28 13:19:40 +0200

    Add quest SQL Bernardo

--- TEST_DATA_ENGINEER/Requête_SQL_Bernardo.sql
+++ TEST_DATA_ENGINEER/Requête_SQL_Bernardo.sql
@@ -1,1 +1,9 @@
+use dataengineer;
 
+SELECT c.last_name, c.first_name, COUNT(r.customer_id) AS "nombre_de_loc", CONCAT(address, " ", postal_code," ", city) as postal_adress, a.latitude, a.longitude 
+FROM rental r 
+INNER JOIN customer c ON c.customer_id = r.customer_id 
+inner join address a on a.address_id = c.address_id
+GROUP BY r.customer_id 
+ORDER BY nombre_de_loc desc
+limit 1;
